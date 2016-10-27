df65633dd12478cd0fd5ad6bf20c04abb09c1442
sovrasov <sovrasov.vlad@gmail.com>
2016-10-27 22:36:55 +0300

    Bugfix in lab4

--- scripts/lab4.py
+++ scripts/lab4.py
@@ -22,10 +22,10 @@ def plotRhoHist(data, xLabel, yLabel, fileName, color):
     c = np.amax(n) / 2.
     f = np.vectorize(lambda x: c*np.sqrt(4. - x**2))
     lGrid = np.linspace(-2., 2., 100)
-    plt.plot(lGrid, f(lGrid), color[0] + '-')
+    plt.plot(lGrid, f(lGrid), 'b' + '--')
 
     plt.grid()
-    plt.savefig(fileName, format = 'png', dpi = 200)
+    plt.savefig(fileName, format = 'pdf')
     plt.clf()
 
 def plotLevelSpacingsHist(data, xLabel, yLabel, testDistr,
@@ -38,10 +38,10 @@ def plotLevelSpacingsHist(data, xLabel, yLabel, testDistr,
     c = np.amax(n) / testDistr(distrArgMax)
     f = np.vectorize(lambda x: c*testDistr(x))
     sGrid = np.linspace(np.amin(bins), np.amax(bins), 200)
-    plt.plot(sGrid, f(sGrid), color[0] + '-')
+    plt.plot(sGrid, f(sGrid), 'b' + '--')
 
     plt.grid()
-    plt.savefig(fileName, format = 'png', dpi = 200)
+    plt.savefig(fileName, format = 'pdf')
     plt.clf()
 
 def main():
@@ -61,30 +61,32 @@ def main():
     gueAllEigens = np.array(gueEigens).reshape(nImpls*size)
 
     plotRhoHist(goeAllEigens / np.sqrt(size), r'$\lambda$', r'$\rho(\lambda)$',
-        '../pictures/lab4_goe_eig_hist.png', 'green')
+        '../pictures/lab4_goe_eig_hist.pdf', 'green')
     plotRhoHist(gueAllEigens / np.sqrt(size), r'$\lambda$', r'$\rho(\lambda)$',
-        '../pictures/lab4_gue_eig_hist.png', 'red')
+        '../pictures/lab4_gue_eig_hist.pdf', 'red')
 
     goeSplits = []
     for eigenSet in goeEigens:
-        goeSplits.append([eigenSet[i+1] - eigenSet[i] for i in range(len(eigenSet) - 1)])
+        goeSplits.append( \
+            [eigenSet[i+1] - eigenSet[i] for i in range(len(eigenSet) - 1)])
         goeSplits[-1] = np.array(goeSplits[-1]) / np.mean(goeSplits[-1])
 
     gueSplits = []
     for eigenSet in gueEigens:
-        gueSplits.append([eigenSet[i+1] - eigenSet[i] for i in range(len(eigenSet) - 1)])
+        gueSplits.append( \
+            [eigenSet[i+1] - eigenSet[i] for i in range(len(eigenSet) - 1)])
         gueSplits[-1] = np.array(gueSplits[-1]) / np.mean(gueSplits[-1])
 
     goeSplits = np.array(goeSplits).reshape(nImpls*len(goeSplits[0]))
     gueSplits = np.array(gueSplits).reshape(nImpls*len(gueSplits[0]))
 
     plotLevelSpacingsHist(goeSplits, r'$\bar{s}$', r'$p(\bar{s})$',
-        lambda x: x*np.exp(-np.pi * 0.25 * x**2), 2. / np.pi,
-        '../pictures/lab4_goe_split_hist.png', 'green')
+        lambda x: x*np.exp(-np.pi * 0.25 * x**2), np.sqrt(2. / np.pi),
+        '../pictures/lab4_goe_spacing_hist.pdf', 'green')
 
     plotLevelSpacingsHist(gueSplits, r'$\bar{s}$', r'$p(\bar{s})$',
         lambda x: x**2*np.exp(-4. / np.pi * x**2), np.sqrt(np.pi) / 2.,
-        '../pictures/lab4_gue_split_hist.png', 'red')
+        '../pictures/lab4_gue_spacing_hist.pdf', 'red')
 
 if __name__ == '__main__':
     main()
